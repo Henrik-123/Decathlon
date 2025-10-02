c79ca13b270923f8cf4dcff95164af5a8af45cf7
Merlin <dock.johnmartin@gmail.com>
2025-10-02 13:00:15 +0200

    Added scoring function for all events. Results should show under correct discipline. Fixed wrong numbers is 1500m and Long Jump code. 18.5 > 1.85 0.13454 > 0.14354 (This issue only affected AppUI I think?)

--- src/main/java/com/example/decathlon/core/ScoringService.java
+++ src/main/java/com/example/decathlon/core/ScoringService.java
@@ -14,7 +14,14 @@ public class ScoringService {
             "100m",    new EventDef("100m",    Type.TRACK, 25.4347, 18.0, 1.81, "s"),
             "longJump", new EventDef("longJump", Type.FIELD, 0.14354, 220.0, 1.4,  "cm"), // expects cm
             "shotPut",  new EventDef("shotPut",  Type.FIELD, 51.39,   1.5,  1.05, "m"),
-            "400m",    new EventDef("400m",    Type.TRACK, 1.53775,  82.0, 1.81, "s")
+            "400m",    new EventDef("400m",    Type.TRACK, 1.53775,  82.0, 1.81, "s"),
+            "1500m",    new EventDef("1500m",    Type.TRACK, 0.03768,  480, 1.85, "m"),
+            "110mHurdles",    new EventDef("110mHurdles",    Type.TRACK, 5.74352,  28.5, 1.92, "s"),
+            "discusThrow",    new EventDef("discusThrow",    Type.FIELD, 12.91,  4, 1.1, "m"),
+            "javelinThrow",    new EventDef("javelinThrow",    Type.FIELD, 10.14,  7, 1.081, "m"),
+            "highJump",    new EventDef("highJump",    Type.FIELD, 0.8465,  75, 1.42, "cm"),
+            "poleVault",    new EventDef("poleVault",    Type.FIELD, 0.2797,  100, 1.35, "cm")
+
     );
 
     public EventDef get(String id) { return events.get(id); }
